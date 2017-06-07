28482bcf604ad7056bd4a16e832b64e6a3210357
Андрей Овчаренко <ovcharenkoav@meta.ua>
2017-06-08 01:01:05 +0300

    graphiql+

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
 require('babel-register')({
-  "presets": [
-    "es2015",
-    "es2017",
-    //    "react",
-    "stage-0"
+  'presets': [
+    'es2015',
+    'es2017',
+    //    'react',
+    'stage-0'
   ],
-  "plugins": [
-    //    "transform-decorators-legacy",
+  'plugins': [
+    //    'transform-decorators-legacy',
   ]
 });
 require('babel-polyfill');
 const {
-  graphql
-} = require('graphql');
-const readline = require('readline');
-const utils = require('./app/utils');
-const {
   mySchema
 } = require('./schema/main.js');
-const rli = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+const {
+  MongoClient
+} = require('mongodb');
+
+
+const graphqlHTTP = require('express-graphql');
+const express = require('express');
+const app = express();
+const MONGO_URL = 'mongodb://localhost:27017/test';
+MongoClient.connect(MONGO_URL, (err, db) => {
+  if (err) {
+    throw new Error('Fail connect to mongodb');
+  } else {
+    app.use('/graphql', graphqlHTTP({
+      schema: mySchema,
+      context: {
+        db
+      },
+      graphiql: true,
+    }));
+  }
 });
 
-rli.question('Client Request: ', async inputQuery => {
-  const result = await graphql(mySchema, inputQuery);
-  console.log('Server Answer :', result.data);
-  const [bb] = await utils.promi(rli, rli.question, 'what another');
-  rli.close();
-  console.log(bb);
-});
+app.listen(3000, () =>
+  console.log('Running Express.js on port 3000'));
--- schema/main.js
+++ schema/main.js
@@ -1,9 +1,27 @@
 import {
   GraphQLSchema,
   GraphQLObjectType,
-  GraphQLString
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList,
 } from 'graphql';
 
+import {
+  MongoClient
+} from 'mongodb';
+import assert from 'assert';
+import {promisay} from '../app/utils';
+const MONGO_URL = 'mongodb://localhost:27017/test';
+
+promisay(MongoClient, MongoClient.connect, MONGO_URL)
+.then(db => {
+  console.log('Connected to MongoDB server');
+  // The readline interface code
+}, err =>
+  console.log(err)
+);
+
+const roll = () => Math.floor(6 * Math.random()) + 1;
 
 const queryType = new GraphQLObjectType({
   name: 'RootQuery',
@@ -11,6 +29,22 @@ const queryType = new GraphQLObjectType({
     hello: {
       type: GraphQLString,
       resolve: () => 'world'
+    },
+    diceRoll: {
+      type: new GraphQLList(GraphQLInt),
+      args: {
+        count: {
+          type: GraphQLInt,
+          defaultValue: 2,
+        },
+      },
+      resolve: (_, args) => {
+        let rolls = [];
+        for (let i = 0; i < args.count; i++) {
+          rolls.push(roll());
+        }
+        return rolls;
+      }
     },
   },
 });
